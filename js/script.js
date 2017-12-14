6b8ba104a4336cde17112720c88f04aa21e5727b
Peter Jablonski <pwjablonski@gmail.com>
2017-12-14 11:47:17 -0500

    Update and rename script.js to js/script.js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Pig Latin takes the first consonant (or consonant cluster) of an English 
+// word, moves it to the end of the word and suffixes an ay, or if a word
+// begins with a vowel you just add ay to the end. For example, pig becomes
+// igpay, banana becomes ananabay, and aadvark becomes aadvarkay.
+// CREATE THE FUNCTIONS BELOW
+
+// Document Ready Function. 
+	
+
+
+
+
+
+// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
+
+
+
+
+
+
+// Create the sentenceToPigLatin function that takes a sentence as a parameter
+	//Iterates through all the words in the sentence and transforms each word to pig latin
+	//It should return a transfromed sentance
+
+
