5a077415a278d488eff64f7527e92ac6dad41478
Peter Jablonski <pwjablonski@gmail.com>
2018-06-18 11:17:12 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@
 // word, moves it to the end of the word and suffixes an ay, or if a word
 // begins with a vowel you just add ay to the end. For example, pig becomes
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkay.
-// CREATE THE FUNCTIONS BELOW
 
-// Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
   
 
@@ -13,11 +11,18 @@ $( document ).ready(function() {
 });
 
 
-// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
+// Create the takes a word as a parameter and returns a transfromed word. 
+function wordToPigLatin(word) {
+	var vowels = [ 'a', 'e', 'i', 'o', 'u' ];
 
+	var firstLetter = word[0];
 
-
-
+	if (vowels.indexOf(firstLetter) > -1) {
+		return word + 'ay';
+	} else {
+		return word.slice(1) + firstLetter + 'ay';
+	}
+}
 
 
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
